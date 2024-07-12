931db7580be573057d7e32bb35281bcdb84b0a89
Ricardo Vasquez <r.vfarias@hotmail.com>
2024-07-12 11:18:37 -0400

    version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
-$(document).ready(function(){
-    $('btn').tooltip();   
-  });
-  
+//tooltip//
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+// alerta al presionar "enviar"//
+function alertabtn() {
+  alert("Felicidades, Te contactaremos en unos segundos!!!");
+}
+
+// animaciones para cards DESTACADOS//
+function saltocard() {
+  MouseEvent(mouseover)
+
+}
